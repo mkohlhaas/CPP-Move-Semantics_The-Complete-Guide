048127b209f110402cc516b52017cdc9fc6107e7
Hans-J. Schmid <knock@myopendoor.de>
2025-03-15 19:23:50 +0100

    meson build for lib

---
+++ lib/build.sh
@@ -0,0 +1,33 @@
+cd moveitor
+meson compile -C build
+cd ..
+cd removeif
+meson compile -C build
+cd ..
+cd stringmoveassign
+meson compile -C build
+cd ..
+cd contmove
+meson compile -C build
+cd ..
+cd movealgo
+meson compile -C build
+cd ..
+cd foreachmove
+meson compile -C build
+cd ..
+cd outfile
+meson compile -C build
+cd ..
+cd thread
+meson compile -C build
+cd ..
+cd uniqueptr1
+meson compile -C build
+cd ..
+cd moveonly
+meson compile -C build
+cd ..
+cd future
+meson compile -C build
+cd ..
