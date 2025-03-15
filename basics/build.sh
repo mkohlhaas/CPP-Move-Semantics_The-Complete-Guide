2456f738e752c202513193b0708e8bfc24f0384a
Hans-J. Schmid <knock@myopendoor.de>
2025-03-15 19:10:14 +0100

    meson build for basics

---
+++ basics/build.sh
@@ -0,0 +1,81 @@
+cd card
+meson compile -C build
+cd ..
+cd customer1
+meson compile -C build
+cd ..
+cd customer2
+meson compile -C build
+cd ..
+cd customerimpl
+meson compile -C build
+cd ..
+cd initall
+meson compile -C build
+cd ..
+cd initallperf
+meson compile -C build
+cd ..
+cd initbigperf
+meson compile -C build
+cd ..
+cd initclassic
+meson compile -C build
+cd ..
+cd initclassicperf
+meson compile -C build
+cd ..
+cd initmeasure
+meson compile -C build
+cd ..
+cd initmove
+meson compile -C build
+cd ..
+cd initmoveperf
+meson compile -C build
+cd ..
+cd initperf
+meson compile -C build
+cd ..
+cd intstring
+meson compile -C build
+cd ..
+cd members
+meson compile -C build
+cd ..
+cd motiv03
+meson compile -C build
+cd ..
+cd motiv11
+meson compile -C build
+cd ..
+cd movenoexcept
+meson compile -C build
+cd ..
+cd personcond
+meson compile -C build
+cd ..
+cd person
+meson compile -C build
+cd ..
+cd persondefault
+meson compile -C build
+cd ..
+cd personmove
+meson compile -C build
+cd ..
+cd refqual
+meson compile -C build
+cd ..
+cd sharedint
+meson compile -C build
+cd ..
+cd specialnoexcept
+meson compile -C build
+cd ..
+cd tasksbug
+meson compile -C build
+cd ..
+cd tasks
+meson compile -C build
+cd ..
