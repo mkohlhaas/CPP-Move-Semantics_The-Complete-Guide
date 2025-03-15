0f4df2f51e201d699ce0d891cd3e5824595ab110
Hans-J. Schmid <knock@myopendoor.de>
2025-03-15 19:16:40 +0100

    meson build for generic

---
+++ generic/build.sh
@@ -0,0 +1,27 @@
+cd perfectpassing
+meson compile -C build
+cd ..
+cd universalspec
+meson compile -C build
+cd ..
+cd universalconstructor
+meson compile -C build
+cd ..
+cd perfectautorefref
+meson compile -C build
+cd ..
+cd universalclass
+meson compile -C build
+cd ..
+cd universalconst
+meson compile -C build
+cd ..
+cd call
+meson compile -C build
+cd ..
+cd universalmem
+meson compile -C build
+cd ..
+cd universaltype
+meson compile -C build
+cd ..
