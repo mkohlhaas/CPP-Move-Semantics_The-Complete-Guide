58ee3d5aa27632811dba7b1fbccfc82269d44971
Hans-J. Schmid <knock@myopendoor.de>
2025-03-15 18:44:26 +0100

    remove license bla bla bla

--- lib/outfile.cpp
+++ lib/outfile.cpp
@@ -1,14 +1,3 @@
-//********************************************************
-// The following code example is taken from the book
-//  C++ Move Semantics - The Complete Guide
-//  by Nicolai M. Josuttis (www.josuttis.com)
-//  http://www.cppmove.com
-//
-// The code is licensed under a
-//  Creative Commons Attribution 4.0 International License
-//  http://creativecommons.org/licenses/by/4.0/
-//********************************************************
-
 #include <fstream>
 #include <iostream>
 #include <stream>
--- poly/isnothrowmovable.hpp
+++ poly/isnothrowmovable.hpp
@@ -1,14 +1,3 @@
-//********************************************************
-// The following code example is taken from the book
-//  C++ Move Semantics - The Complete Guide
-//  by Nicolai M. Josuttis (www.josuttis.com)
-//  http://www.cppmove.com
-//
-// The code is licensed under a
-//  Creative Commons Attribution 4.0 International License
-//  http://creativecommons.org/licenses/by/4.0/
-//********************************************************
-
 // type trait to check whether a base class guarantees not to throw
 // in the move constructor (even if the constructor is not callable)
 #ifndef IS_NOTHROW_MOVABLE_HPP
